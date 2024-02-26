e73379221ce1227d89af8dd627e34cb3655e8d96
danzavyalov <146368133+danzavyalov@users.noreply.github.com>
2024-02-25 20:42:00 -0500

    Add files via upload

---
+++ Week 4 Prep/user.js
@@ -0,0 +1,21 @@
+const postListEl = document.querySelector('.post-list')
+
+async function main() {
+    const id = localStorage.getItem("id")
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
+    const postsData = await posts.json()
+    console.log(postsData)
+
+    postsData.map(post => `
+    <div class="post">
+    <div class="post__title">
+      Post Title
+    </div>
+    <p class="post__body">
+      Post Body
+    </p>
+  </div>
+  `)
+}
+
+main()
